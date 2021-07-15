a896142c428525268167a8ad1bb575394ad52fe7
khanh <gundamaster5@gmail.com>
2021-07-15 21:30:29 +0700

    1st commit

---
+++ get_ions_account_participating_in_gaia_gov.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+url_validators = "https://api.cosmostation.io/v1/staking/validators"
+r_validators = requests.get(url=url_validators)
+all_validators = r_validators.json()
+
+moniker_to_addr = {}
+for v in all_validators:
+    moniker_to_addr[v['moniker']] = v['account_address']
+
+f1 = open("ions.json", "r")
+l_ion_account_with_amount = json.load(f1)
+l_ion_accounts = set(l_ion_account_with_amount.keys())
+
+
+f2 = open("votes.jsonl", "r")
+json_list = list(f2)
+l_gov_proposals = []
+s_gov_account = set()
+for json_str in json_list:
+    result = json.loads(json_str)
+    l_gov_proposals.append(result)
+
+for proposal in l_gov_proposals:
+    votes = proposal['votes']
+    for vote in votes:
+        if vote["voter"] == "":
+            if moniker_to_addr.get(vote["moniker"]) is None:
+                s_gov_account.add(vote["moniker"])
+            else:
+                s_gov_account.add(moniker_to_addr[vote["moniker"]])
+        else :
+            s_gov_account.add("voter")
+
+g = open("ions_account_that_participate_in_cosmos_gov.json", "w")
+json.dump(l_ion_accounts.intersection(s_gov_account), g)
---
+++ scrape-to-get-all-gaia-proposals-votes.py
@@ -0,0 +1,111 @@
+import json
+import logging
+import re
+import time
+import traceback
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+import requests
+
+
+f = open("votes.jsonl", 'w')
+
+
+url_proposals = "https://api.cosmostation.io/v1/gov/proposals"
+r_proposals = requests.get(url=url_proposals)
+all_proposals = r_proposals.json()
+
+
+last_proposal_id = max(all_proposals, key=lambda x: x["proposal_id"])["proposal_id"]
+
+
+
+url_proposal_from_cosmostation = "https://api.cosmostation.io/v1/gov/proposal/votes/"
+url_proposal_from_hubble = "https://hubble.figment.io/cosmos/chains/cosmoshub-version/governance/proposals/"
+
+version_map = ['1']*6 + ['2']*14 + ['3']*18 + ['4']*1000
+
+for id in range(1, last_proposal_id+1):
+    print(url_proposal_from_cosmostation + str(id))
+    r_proposal_id = requests.get(url=url_proposal_from_cosmostation + str(id))
+    proposal_id = r_proposal_id.json()
+    try :
+        e = proposal_id["error_code"]
+        print(e)
+        time.sleep(2)
+        continue
+    except KeyError:
+        time.sleep(2)
+        pass
+    if len(proposal_id["votes"]) != 0:
+        json.dump(proposal_id, f)
+        f.write('\n')
+        time.sleep(2)
+    else:
+
+        url = url_proposal_from_hubble.replace('version', version_map[id]) + str(id)
+        try:
+            document = urlopen(url)
+        except:
+
+            continue
+
+
+
+
+        html = document.read()
+
+        soup = BeautifulSoup(html, "html.parser")
+        proposal_id = {}
+
+        tally = {"yes_num": 0, "abstain_num": 0, "no_num": 0, "no_with_veto_num": 0}
+        vote_options_and_amount = soup.findAll("div", {"class": re.compile(r"tally flex-column align-items-center*")})
+        vote_options = ["yes_amount", "abstain_amount", "no_amount", "no_with_veto_amount"]
+
+        for id, vo in enumerate(vote_options_and_amount):
+
+
+            tally[vote_options[id]] = vo.find('span').contents[0]
+
+
+
+
+        votes = soup.findAll("tr", {"data-vote-id": re.compile(r".*")})
+        v_list = []
+        for i in votes:
+
+            vote = {}
+
+            vote["voter"] = ""
+
+            vote["moniker"] = ""
+
+            vote["option"] = "VOTE_OPTION_" + i.find("td").contents[0].upper()
+            if vote["option"] == "VOTE_OPTION_YES":
+                tally["yes_num"] += 1
+            elif vote["option"] == "VOTE_OPTION_ABSTAIN":
+                tally["abstain_num"] += 1
+            elif vote["option"] == "VOTE_OPTION_NO":
+                tally["no_num"] += 1
+            else :
+                tally["no_with_veto_num"] += 1
+            try:
+                vote["voter"] = i.find('a').span.contents[0]
+            except AttributeError:
+                vote["moniker"] = i.find('a').contents[0].strip()
+
+            vote["tx_hash"] = ""
+            vote["time"] = ""
+            v_list.append(vote)
+        proposal_id["tally"] = tally
+        proposal_id["votes"] = v_list
+        json.dump(proposal_id, f)
+        f.write('\n')
+        time.sleep(2)
+
+
+
+
+
+
